9f0cb77e25e4ce719a2db2e2ce382bd4d2a6c722
PembaSrpa <nirashanichang@gmail.com>
2026-05-05 18:31:37 +0545

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,18 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "MEMENTO MORI",
   description: "REMEMBER YOU MUST DIE",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
